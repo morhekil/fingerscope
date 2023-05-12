1c9805b7bcc1aeaa06f266b10f479328c6f09a6a
Oleg Ivanov <morhekil@morhekil.net>
2023-05-12 22:59:07 +1000

    climb selection view

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Link from 'next/link'
 
-import { Footer } from './Footer'
-import { Header } from './Header'
+import { Footer } from '@/app/Footer'
+import { Header } from '@/app/Header'
 import { Logo } from '@/components/Logo'
 // import { Navigation } from '@/components/Navigation'
 // import { Prose } from '@/components/Prose'
 
-export default function Home() {
+import * as store from '@/store'
+
+const sbsOwnerID = 'YToMTO4GhbbbAshq4I5rdPyQZyu2'
+
+export default async function Home() {
+  const competitions = await store.competitions(sbsOwnerID)
+
   return (
     <div className="lg:ml-72 xl:ml-80">
       <header className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex">
@@ -21,9 +27,37 @@ export default function Home() {
         </div>
       </header>
       <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
-        <main className="py-16">
-          {/* <Prose as="article">{children}</Prose> */}
-          Auth form here
+        <main className="py-8">
+          <ul role="list" className="divide-y divide-gray-100">
+            {competitions.map((comp) => (
+              <li key={comp.id} className="flex justify-between gap-x-6 py-5">
+                <div className="flex gap-x-4">
+                  <div className="min-w-0 flex-auto">
+                    <Link href={`/${comp.id}`}>
+                      <p className="text-sm font-semibold leading-6 text-gray-900">
+                        {comp.name}
+                      </p>
+                      <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                        {new Date(
+                          comp.created.seconds * 1000
+                        ).toLocaleDateString()}
+                      </p>
+                    </Link>
+                  </div>
+                </div>
+                <div className="hidden sm:flex sm:flex-col sm:items-end">
+                  <p className="text-sm leading-6 text-gray-900">
+                    <Link
+                      href={`/${comp.id}`}
+                      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Open
+                    </Link>
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
         </main>
         <Footer />
       </div>
--- store/competitions.tsx
+++ store/competitions.tsx
@@ -1,19 +1,13 @@
-import {
-  Firestore,
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-} from "firebase/firestore";
-import { db } from "./firebase";
+import { Firestore, collection, doc, getDoc, getDocs } from 'firebase/firestore'
+import { db } from './firebase'
 
 const competition = async (id: string) => {
-  const path = `competitions/${id}`;
-  const resref = doc(db, path);
-  return (await getDoc(resref)).data();
-};
+  const path = `competitions/${id}`
+  const resref = doc(db, path)
+  return (await getDoc(resref)).data()!
+}
 
-export { competition };
+export { competition }
 
 const competitions = async (ownerID: string) =>
   (await getDocs(collection(db, `competitions`))).docs
@@ -21,23 +15,23 @@ const competitions = async (ownerID: string) =>
     .map((doc) => ({ id: doc.id, ...doc.data() }))
     // @ts-ignore
     .map(({ id, name, created }) => ({ id, name, created }))
-    .sort((a, b) => b.created - a.created);
+    .sort((a, b) => b.created - a.created)
 
 const categories = async (comp: string) =>
   (await getDocs(collection(db, `competitions/${comp}/categories`))).docs.map(
     (doc) => doc.id
-  );
+  )
 
 const climbs = async (comp: string) =>
   (await getDocs(collection(db, `competitions/${comp}/climbs`))).docs
     .map((doc) => doc.data())
     .map(({ climbNo, marking, score }) => ({ climbNo, marking, score }))
-    .sort((a, b) => b.score - a.score);
+    .sort((a, b) => b.score - a.score)
 
 const quals = async (comp: string) =>
   (
     await getDocs(collection(db, `competitions/${comp}/qualificationScores`))
-  ).docs.map((doc) => doc.data());
+  ).docs.map((doc) => doc.data())
 
 const competitors = async (comp: string) =>
   (await getDocs(collection(db, `competitions/${comp}/competitors`))).docs
@@ -46,6 +40,6 @@ const competitors = async (comp: string) =>
       name: firstName,
       no: competitorNo,
       category,
-    }));
+    }))
 
-export { competitions, categories, climbs, quals, competitors };
+export { competitions, categories, climbs, quals, competitors }
