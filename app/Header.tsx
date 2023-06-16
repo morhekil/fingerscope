d2a444d106cdf97853b594a0a0d68df4ce8033c7
Oleg Ivanov <morhekil@morhekil.net>
2023-06-16 13:20:24 +1000

    sticky header for mobile

--- app/Header.tsx
+++ app/Header.tsx
@@ -15,7 +15,7 @@ export function Header({
         <div
           className={clsx(
             className,
-            'fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80',
+            'sticky inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80',
             'backdrop-blur-sm dark:backdrop-blur lg:left-72 xl:left-80',
             'bg-white/0.9 dark:bg-zinc-900/0.8'
           )}
